9a85dd1fd678d9b3dd0144a315ca8062d5b301dc
Andrew (kaya6) <kaya6@mcmaster.ca>
2023-02-28 16:49:45 -0500

    Added height conversion when different units of measure are used

--- src/HeightInput.js
+++ src/HeightInput.js
@@ -1,42 +1,64 @@
 import React from "react";
 import { useState } from "react";
 
+const getMetricFromImperial = (feet, inches) => {
+    let totalInches = (parseInt(feet) * 12) + parseInt(inches);
+    let cm = totalInches * 2.54;
+    return cm;
+
+}
+
+const getImperialFromMetric = (cm) => {
+    let inches = cm/2.54;
+    return [Math.floor(inches/12), inches%12];
+}
+
 const HeightInput = (props) => {
     const [feet, setFeet] = useState('');
     const [inches, setInches] = useState('');
     const [cm, setCm] = useState('');
 
     const handleFeetChange = (event) => {
+        let newCm = getMetricFromImperial(event.target.value, inches);
         setFeet(event.target.value);
+        setCm(newCm);
         props.setHeightContents({
             imperialHeight: {
                 feet: event.target.value,
                 inches: inches
             },
             metricHeight: {
-                cm: cm
+                cm: newCm
             }
         });
     };
 
     const handleInchesChange = (event) => {
         setInches(event.target.value);
+        let newCm = getMetricFromImperial(feet, event.target.value);
+        console.log(newCm)
+        setCm(newCm);
         props.setHeightContents({
             imperialHeight: {
                 feet: feet,
                 inches: event.target.value
             },
             metricHeight: {
-                cm: cm
+                cm: newCm
             }
         });
     };
     const handleCmChange = (event) => {
         setCm(event.target.value);
+        let newFeetAndInches = getImperialFromMetric(event.target.value);
+        let newFeet = newFeetAndInches[0];
+        let newInches = newFeetAndInches[1];
+        setInches(newInches);
+        setFeet(newFeet);
         props.setHeightContents({
             imperialHeight: {
-                feet: feet,
-                inches: inches
+                feet: newFeet,
+                inches: newInches
             },
             metricHeight: {
                 cm: event.target.value
